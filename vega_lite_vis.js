8a6138a80800898ab0de7b8bb7ec75955e93806c
tmar0048 <127160650+tmar0048@users.noreply.github.com>
2024-10-08 16:10:11 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,12 @@
 var vg_1 = "visualization.vl.json";
 var vg_2 = "petrolprices.vl.json";
+var vg_3 = "trainfares.vl.json";
 vegaEmbed("#AusMap", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#PetrolLine", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#TrainMap", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
