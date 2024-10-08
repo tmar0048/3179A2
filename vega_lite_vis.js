0501b61408e8f88a2442e985eb4aceb2c0c94750
tmar0048 <127160650+tmar0048@users.noreply.github.com>
2024-10-08 20:58:58 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,8 @@
 var vg_1 = "visualization.vl.json";
 var vg_2 = "petrolprices.vl.json";
 var vg_3 = "trainfares.vl.json";
+var vg_4 = "ebike.vl.json";
+
 vegaEmbed("#AusMap", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -8,5 +10,8 @@ vegaEmbed("#PetrolLine", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#TrainMap", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#Ebike", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
